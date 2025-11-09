183a76ebc5099b3cfb4b80b3b9a2d8c0c84d6f58
R1DDL30510 <a.krinov@mail.ru>
2025-11-09 06:10:12 +0100

    ✨ feat: Улучшено проверка линтером с использованием исключений для обработки ошибок

--- 8) scripts/lint.ps1
+++ 8) scripts/lint.ps1
@@ -1,6 +1,9 @@
-ruff check . || pyflakes .
-if ($LASTEXITCODE -ne 0) {
-    Write-Output "Линтерские ошибки обнаружены."
-    exit 1
+try {
+    ruff check . -ErrorAction Stop
+} catch {
+    pyflakes .
+    if ($LASTEXITCODE -ne 0) {
+        Write-Output "Линтерские ошибки обнаружены."
+        exit 1
+    }
 }
-Write-Output "Нет линтерских ошибок."
